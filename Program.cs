e9c2e4a6b57d2efebac6fd3df96c8230b6c6dd99
Ana Francisca <anafranciscachavesbarreiros@gmail.com>
2025-10-03 20:29:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("--- Media Aritmedica de três ---");
+
+double nota1, nota2, nota3, media;
+
+Console.Write("Acressente o primeiro numero: ");
+nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Acressente o segundo numero: ");
+nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Acressente o terceiro número: ");
+nota3 = Convert.ToDouble(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine($"\nMédia: {media:N1}");
